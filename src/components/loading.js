92dc2a2834414db0672a3f9ef65b9b369297fec0
Rebecka Kinn <rebeckaki@getacademy.no>
2026-06-17 10:18:35 +0200

    Added the post-css in a seperate file

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,7 +1,9 @@
 export default function LoadingPost() {
   return /*html*/ `
     <section class="post-card">
-      
+      <div></div>
+      <div></div>
+      <div></div>
     </section>
   `;
 }
