c12b0495f540646d801678b74a10ef478f3819d2
Rebecka Kinn <rebeckaki@getacademy.no>
2026-06-17 10:15:44 +0200

    Made post css mostly ready, added icons
    Made several CSS ajustments, including main margin in media query, post-elements and starting on the new loading element. Added icons SVG I added from a free template in figma.

--- src/components/loading.js
+++ src/components/loading.js
@@ -1,10 +1,7 @@
-export default function LoadingPost(title = "loading", message = "") {
+export default function LoadingPost() {
   return /*html*/ `
-    <section class="flex center">
-      <div class="flex column center gap-1">
-        <h1>${title}</h1>
-        <p>${message}</p>
-      </div>
+    <section class="post-card">
+      
     </section>
   `;
 }
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,13 +1,16 @@
 import { getPosts } from "../api/posts.js";
 import LoadingPost from "../components/loading.js";
 import Fallback from "./fallback.js";
+import commentIcon from "../assets/icons/comment.svg";
+import reactionIcon from "../assets/icons/reaction.svg";
 
 export default function FeedPage() {
   return /*html*/ `
     <h1>pinkripple</h1>
     <h2>Welcome to pinkripple!</h2>
     <p>Explore the latest posts and connect with your network.</p>
-    <section id="posts-feed" class="flex column gap-2" aria-live="polite">
+    ${LoadingPost()}
+    <section id="posts-feed" class="flex column align-center gap-2" aria-live="polite">
     </section>
   `;
 }
@@ -52,27 +55,8 @@ function createPostCard(post) {
     <article data-post-id="${post.id}" class="post-card">
 
       <section class="flex column">
-          <h3>${post.title || "Untitled"}</h3>
-        
-          <section>
-          <div>
-            <span>created:</span>
-            <span>${formatDate(post.created)}</span>
-          </div>
-          ${
-            !checkIsUpdated(post.created, post.updated)
-              ? /*HTML*/ `
-            <div>
-              <span>updated:</span>
-              <span>${formatDate(post.updated)}</span>
-            </div>
-            `
-              : ""
-          }
-        </section>
-        </section>
-
-
+        <h3 class="post-title">${post.title || "Untitled"}</h3>
+      </section>
 
       <section>
         <p>${post.body || ""}</p>
@@ -85,21 +69,42 @@ function createPostCard(post) {
           `
             : ""
         }
+        <section class="post-timestamp flex">
+            <div>
+              <span>created:</span>
+              <span>${formatDate(post.created)}</span>
+            </div>
+             ${
+               !checkIsUpdated(post.created, post.updated)
+                 ? /*HTML*/ `
+              <span class="separator">|</span>
+              <div>
+                <span>updated:</span>
+                <span>${formatDate(post.updated)}</span>
+              </div>
+            `
+                 : ""
+             }
+        </section>
       </section>
-      
+      <section>
         <ul class="post-tags flex gap-1">${generateTags(post.tags)}</ul>
-     
-      <section class="flex gap-2">
-        <div>
-          <span>&#x1F5E8;</span>
-          <span>${post._count.comments}</span>
-        </div>
-        <div>
-          <span>&#9786;</span>
-          <span>${post._count.reactions}</span>
-        </div>
+      </section>
+      <section class="post-count flex gap-2">
+        ${generateSocialCount(post._count.comments, commentIcon, "Comments")}
+        ${generateSocialCount(post._count.reactions, reactionIcon, "Reactions")}
       </section>
     </article>
+  `;
+}
+function generateSocialCount(count, imgSrc, imgAlt) {
+  return /*HTML*/ `
+    <a class="count-element flex center">
+      <div class="flex">
+        <img src="${imgSrc}" alt="${imgAlt}" class="icon">
+      </div>
+      <span>${count}</span>
+    </a>
   `;
 }
 
